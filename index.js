1ae2dc59ccf99cb8965a584935006e298947df1c
kzhctech <95127984+kzhctech@users.noreply.github.com>
2021-12-02 17:19:13 +0600

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ app.listen(process.env.PORT, function () {
 var path = require('path');
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var io = require('socket.io')(http,{
+    cors: { origin: "*" }
+});
 app.get('/', (req, res) => {
   console.error('express connection');
   res.sendFile(path.join(__dirname, 'index.html'));
