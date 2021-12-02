09017cd79952655fceeeba5a93ac0d82157ad1fb
kzhctech <95127984+kzhctech@users.noreply.github.com>
2021-12-02 17:08:38 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 var express = require('express')
 var app = express()
 
@@ -8,3 +9,24 @@ app.get('/', function (req, res) {
 app.listen(process.env.PORT, function () {
   console.log('Listening on port 3000...')
 })
+*/
+var path = require('path');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+app.get('/', (req, res) => {
+  console.error('express connection');
+  res.sendFile(path.join(__dirname, 'si.html'));
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('message', (message) =>     {
+        console.log(message);
+        io.emit('message', `${socket.id.substr(0,2)} said ${message}` );   
+    });
+});
+
+http.listen(process.env.PORT, () => console.error('listening on http://localhost:3002/'));
+console.error('socket.io example');
