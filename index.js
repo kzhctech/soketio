be3ab8b54e8a153c88d5e83126ac00f5a9ae9fa3
kzhctech <95127984+kzhctech@users.noreply.github.com>
2021-12-09 07:01:38 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,60 @@
+const express = require('express');
+const path = require('path');
+const cheerio = require('cheerio');
+const axios = require('axios');
 
+
+
+axios.get('https://m.cricbuzz.com/cricket-commentary/36332/aus-vs-eng-1st-test-the-ashes-2021-22').then((response) => {
+  // Load the web page source code into a cheerio instance
+  const $ = cheerio.load(response.data);
+  const urlElems = $('.list-content span:nth-child(5)').text();
+  const status = $('.cbz-ui-status').text();
+
+  const batsman1name = $('#top table tr:nth-child(2)').first().first().find('td:nth-child(1)').text();
+  const batsman2name = $('#top table tr:nth-child(3)').first().first().find('td:nth-child(1)').text();
+  const bowlername = $('#top').find('div:nth-child(11)').find('div:nth-child(3)').find('tr:nth-child(2)').find('td:nth-child(1)').text();
+
+
+  const batsman1run = $('#top table tr:nth-child(2)').first().first().find('td:nth-child(2)').text();
+  const batsman2run = $('#top table tr:nth-child(3)').first().first().find('td:nth-child(2)').text();
+  const bowlerwikwt = $('#top').find('div:nth-child(11)').find('div:nth-child(3)').find('tr:nth-child(2)').find('td:nth-child(5)').text();
+  const bowlerover = $('#top').find('div:nth-child(11)').find('div:nth-child(3)').find('tr:nth-child(2)').find('td:nth-child(2)').text();
+
+
+  const batTeam = $('#top h3.ui-li-heading span.miniscore-teams.ui-bat-team-scores').text();
+  const bowlTeam = $('#top h3.ui-li-heading span.teamscores.ui-bowl-team-scores').text();
+  const crr = $('#top .ui-match-scores-branding .crr').text();
+  const commentry = $('#paginationList').first().first().first().first().children().first().first().children().children().children().children().children().first().text();
+  
+  console.log('Status:',status);
+  console.log(' ');
+  console.log('Bat:',batTeam,'CRR:',crr);
+  console.log(' ');
+  console.log(batsman1name,'Run:',batsman1run);
+  console.log(' ');
+  console.log(batsman2name,'Run:',batsman2run);
+  console.log(' ');
+  console.log(bowlername,'Over',bowlerover,'Wicket',bowlerwikwt);
+  console.log(' ');
+  console.log(commentry)
+
+var app = express()
+app.get('/', function (req, res) {
+   res.send( `Status is: ${status} Run is: ${batTeam}`);
+   //res.sendFile(path.join(__dirname, 'index.html'));
+})
+
+app.listen(, function () {
+  console.log('Listening on port 3000...');
+})
+
+
+
+
+
+
+/*
 const cheerio = require('cheerio');
 const axios = require('axios');
 
@@ -51,7 +107,9 @@ io.on('connection', (socket) => {
     });
 });
 
-*/
+
 
 http.listen(process.env.PORT, () => console.error('listening on http://localhost:3002/'));
 console.error('socket.io example');
+
+*/
