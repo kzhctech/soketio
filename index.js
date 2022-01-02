d631fb93a1c73aab48814ce61170aa72e199bc98
kzhctech <95127984+kzhctech@users.noreply.github.com>
2022-01-02 04:00:21 +0000

    Update index.js

--- index.js
+++ index.js
@@ -72,6 +72,53 @@ axios.get('https://m.cricbuzz.com/cricket-commentary/40536/nz-vs-ban-1st-test-ba
 
     
 io.on('connection', (socket) => {
+    
+    
+      let cmnty = '';
+    
+  axios.get('https://m.cricbuzz.com/cricket-commentary/40536/nz-vs-ban-1st-test-bangladesh-tour-of-new-zealand-2022').then((response) => {
+      
+  // Load the web page source code into a cheerio instance
+      
+  const $ = cheerio.load(response.data);
+ 
+  const status = $('.cbz-ui-status').text();
+
+  const title = $('#top').find('div').find('div:nth-child(9)').find('h4').text();
+  const batsman1name = $('#top table tr:nth-child(2)').first().first().find('td:nth-child(1)').text();
+  const batsman2name = $('#top table tr:nth-child(3)').first().first().find('td:nth-child(1)').text();
+  const bowlername = $('#top').find('div:nth-child(11)').find('div:nth-child(3)').find('tr:nth-child(2)').find('td:nth-child(1)').text();
+
+
+  const batsman1run = $('#top table tr:nth-child(2)').first().first().find('td:nth-child(2)').text();
+  const batsman2run = $('#top table tr:nth-child(3)').first().first().find('td:nth-child(2)').text();
+  const bowlerwikwt = $('#top').find('div:nth-child(11)').find('div:nth-child(3)').find('tr:nth-child(2)').find('td:nth-child(5)').text();
+  const bowlerover = $('#top').find('div:nth-child(11)').find('div:nth-child(3)').find('tr:nth-child(2)').find('td:nth-child(2)').text();
+
+
+  const batTeam = $('#top h3.ui-li-heading span.miniscore-teams.ui-bat-team-scores').text();
+  const bowlTeam = $('#top h3.ui-li-heading span.teamscores.ui-bowl-team-scores').text();
+  const crr = $('#top .ui-match-scores-branding .crr').text();
+  const commentry = $('#paginationList').first().first().first().first().children().first().first().children().children().children().children().children().first().text();
+  const lbb = $('#top').find('div').find('div:nth-child(11)').find('div.cb-list-item.miniscore-data.ui-branding-style.ui-branding-style-partner').find('div').children().children().find('span:nth-child(8)').text();
+  
+      if(commentry != cmnty){
+          cmnty = commentry;
+   io.emit('message', {title,lbb,status,batTeam,commentry,batsman1name,batsman1run,batsman2name,batsman2run,bowlername,bowlerwikwt,bowlerover}); 
+      
+      }
+  
+
+      
+  });
+
+}
+   
+    
+    
+    
+    
+    
   
   console.log('a user connected');
    
