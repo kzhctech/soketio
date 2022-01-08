3f228c81de2043e5f775fce63b962b9b140759c7
kzhctech <95127984+kzhctech@users.noreply.github.com>
2022-01-08 04:09:56 +0000

    Update index.js

--- index.js
+++ index.js
@@ -16,22 +16,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-mongodb.connect('mongodb+srv://kzhccric:2FQHi2IPGWdllW21@cluster0.v6byg.mongodb.net/kzhcCric?retryWrites=true&w=majority', { useUnifiedTopology: true }, function (err, client) {
-app.get("/dbc", async (req, res) => {
-  try {
-    const news = await client.db().collection("news").find({ author: "tajbir" }).toArray()
-    if (news.length) {
-      res.json(news)
-    } else {
-      res.json("You do not currently have any dogs in your pets collection.")
-    }
-  } catch (err) {
-    console.log(err)
-    res.json("Try again later.")
-  }
-})
-});
-
 
 app.get('/db', (req, res) => {
     news = 'hi';
