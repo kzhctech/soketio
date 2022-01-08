c0da741c68f973f96e4ca8a39857c736fe1cce9a
kzhctech <95127984+kzhctech@users.noreply.github.com>
2022-01-08 03:42:15 +0000

    Update index.js

--- index.js
+++ index.js
@@ -17,26 +17,6 @@ app.get('/', (req, res) => {
 });
 
 
-mongodb.connect('mongodb+srv://kzhccric:2FQHi2IPGWdllW21@cluster0.v6byg.mongodb.net/kzhcCric?retryWrites=true&w=majority', { useUnifiedTopology: true }, function (err, client) {
-//news = await db.collection("news").find({ author: "tajbir" }).toArray();
-
-app.get('/db', async (req, res) => {
-  try {
-    const news = await db.collection("news").find({ author: "tajbir" }).toArray()
-    if (news.length) {
-      res.json(news)
-    } else {
-      res.json("You do not currently have any dogs in your pets collection.")
-    }
-  } catch (err) {
-    console.log(err)
-    res.json("Try again later.")
-  }
-});
-
-
-
-
 app.get('/cric', function (req, res) {
 
 
