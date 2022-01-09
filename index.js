299164c1058ea6706540cb8afc58298483ad89fb
kzhctech <95127984+kzhctech@users.noreply.github.com>
2022-01-09 04:09:03 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var MongoClient = require('mongodb');
+var mongodb = require('mongodb');
 const cheerio = require('cheerio');
 const axios = require('axios');
 var cmnty = '';
@@ -21,6 +21,32 @@ app.get('/db', (req, res) => {
     news = 'hi';
     res.send(news);
 });
+
+
+
+
+
+mongodb.connect('mongodb+srv://kzhccric:2FQHi2IPGWdllW21@cluster0.v6byg.mongodb.net/kzhcCric?retryWrites=true&w=majority', { useUnifiedTopology: true }, function (err, client) {
+
+const db = client.db()
+
+app.get("/dbc", async (req, res) => {
+  try {
+    const news = await db.collection("news").find({ author: "tajbir" }).toArray()
+    if (news.length) {
+      res.json(news)
+    } else {
+      res.json("You do not currently have any dogs in your pets collection.")
+    }
+  } catch (err) {
+    console.log(err)
+    res.json("Try again later.")
+  }
+})
+})
+
+
+
 
 
 
