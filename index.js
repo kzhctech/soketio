1e0f527541faa6e719d3a3f146609796cbecf8fc
kzhctech <kzhctech@gmail.com>
2022-03-28 15:19:39 +0600

    hey

---
+++ index.js
@@ -0,0 +1,160 @@
+var mongodb = require('mongodb');
+const cheerio = require('cheerio');
+const axios = require('axios');
+var cmnty = '';
+
+var path = require('path');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http,{
+    cors: { origin: "*" }
+});
+
+
+app.get('/', (req, res) => {
+  console.error('express connection');
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+
+app.get('/db', (req, res) => {
+    news = 'hi';
+    res.send(news);
+});
+
+
+
+
+
+mongodb.connect('mongodb+srv://kzhccric:2FQHi2IPGWdllW21@cluster0.v6byg.mongodb.net/kzhcCric?retryWrites=true&w=majority', { useUnifiedTopology: true }, function (err, client) {
+
+const db = client.db()
+
+app.get("/dbc", async (req, res) => {
+  try {
+    const news = await db.collection("news").find().toArray()
+    if (news.length) {
+      res.json(news)
+    } else {
+      res.json("You do not currently have any dogs in your pets collection.")
+    }
+  } catch (err) {
+    console.log(err)
+    res.json("Try again later.")
+  }
+})
+})
+
+
+
+
+
+
+
+app.get('/cric', function (req, res) {
+
+
+
+
+axios.get('https://m.cricbuzz.com/cricket-commentary/40536/nz-vs-ban-1st-test-bangladesh-tour-of-new-zealand-2022').then((response) => {
+
+  const $ = cheerio.load(response.data);
+
+  const status = $('.cbz-ui-status').text();
+  const title = $('#top').find('div').find('div:nth-child(9)').find('h4').text();
+
+  const batsman1name = $('#top table tr:nth-child(2)').first().first().find('td:nth-child(1)').text();
+  const batsman2name = $('#top table tr:nth-child(3)').first().first().find('td:nth-child(1)').text();
+  const bowlername = $('#top').find('div:nth-child(11)').find('div:nth-child(3)').find('tr:nth-child(2)').find('td:nth-child(1)').text();
+
+
+  const batsman1run = $('#top table tr:nth-child(2)').first().first().find('td:nth-child(2)').text();
+  const batsman2run = $('#top table tr:nth-child(3)').first().first().find('td:nth-child(2)').text();
+  const bowlerwikwt = $('#top').find('div:nth-child(11)').find('div:nth-child(3)').find('tr:nth-child(2)').find('td:nth-child(5)').text();
+  const bowlerover = $('#top').find('div:nth-child(11)').find('div:nth-child(3)').find('tr:nth-child(2)').find('td:nth-child(2)').text();
+
+
+  const batTeam = $('#top h3.ui-li-heading span.miniscore-teams.ui-bat-team-scores').text();
+  const bowlTeam = $('#top h3.ui-li-heading span.teamscores.ui-bowl-team-scores').text();
+  const crr = $('#top .ui-match-scores-branding .crr').text();
+  const lbb = $('#top').find('div').find('div:nth-child(11)').find('div.cb-list-item.miniscore-data.ui-branding-style.ui-branding-style-partner').find('div').children().children().find('span:nth-child(8)').text();
+  const commentry = $('#paginationList').first().first().first().first().children().first().first().children().children().children().children().children().first().text();
+ 
+  console.log('Title:',title);
+  console.log(' ');
+  console.log('Status:',status);
+  console.log(' ');
+  console.log('Bat:',batTeam,'CRR:',crr);
+  console.log(' ');
+  console.log(batsman1name,'Run:',batsman1run);
+  console.log(' ');
+  console.log(batsman2name,'Run:',batsman2run);
+  console.log(' ');
+  console.log(bowlername,'Over',bowlerover,'Wicket',bowlerwikwt);
+  console.log(' ');
+  console.log(commentry);
+
+   res.send( `Status ${status}`);
+   //res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+});
+
+
+
+
+    
+io.on('connection', (socket) => {
+
+ cmnty = '';
+  
+console.log('a user connected');
+   
+setInterval(function () {
+  let linked = 'https://m.cricbuzz.com/cricket-commentary/42931/cgc-vs-klt-eliminator-bangladesh-premier-league-2022';
+  axios.get(linked).then((response) => {
+      
+  // Load the web page source code into a cheerio instance
+
+      
+  const $ = cheerio.load(response.data);
+ 
+  const status = $('.cbz-ui-status').text();
+
+  const title = $('#top').find('div').find('div:nth-child(9)').find('h4').text();
+  const batsman1name = $('#top table tr:nth-child(2)').first().first().find('td:nth-child(1)').text();
+  const batsman2name = $('#top table tr:nth-child(3)').first().first().find('td:nth-child(1)').text();
+  const bowlername = $('#top').find('div:nth-child(11)').find('div:nth-child(3)').find('tr:nth-child(2)').find('td:nth-child(1)').text();
+
+
+  const batsman1run = $('#top table tr:nth-child(2)').first().first().find('td:nth-child(2)').text();
+  const batsman2run = $('#top table tr:nth-child(3)').first().first().find('td:nth-child(2)').text();
+  const bowlerwikwt = $('#top').find('div:nth-child(11)').find('div:nth-child(3)').find('tr:nth-child(2)').find('td:nth-child(5)').text();
+  const bowlerover = $('#top').find('div:nth-child(11)').find('div:nth-child(3)').find('tr:nth-child(2)').find('td:nth-child(2)').text();
+
+
+  const batTeam = $('#top h3.ui-li-heading span.miniscore-teams.ui-bat-team-scores').text();
+  const bowlTeam = $('#top h3.ui-li-heading span.teamscores.ui-bowl-team-scores').text();
+  const crr = $('#top .ui-match-scores-branding .crr').text();
+  const commentry = $('#paginationList').find('div').find('div:nth-child(3)').text();
+  const commentry2 = $('#paginationList').find('div').find('div:nth-child(5)').text();
+  const lbb = $('#top').find('div').find('div:nth-child(11)').find('div.cb-list-item.miniscore-data.ui-branding-style.ui-branding-style-partner').find('div').children().children().find('span:nth-child(8)').text();
+  
+      if(commentry != cmnty){
+          cmnty = commentry;
+   io.emit('message', {title,lbb,status,batTeam,commentry,batsman1name,batsman1run,batsman2name,batsman2run,bowlername,bowlerwikwt,bowlerover}); 
+      
+      }
+  
+
+      
+  });
+
+},10000);
+   
+});
+
+http.listen(process.env.PORT, () => console.error('listening on http://localhost:3002/'));
+console.error('socket.io example');
+
+
--- server.js
+++ server.js
@@ -1,29 +1,184 @@
+var mongoose = require('mongoose');
 const express = require('express');
-const mongoose = require('mongoose');
 const app = express();
+app.use(express.json());
 const ejs = require('ejs');
-const { kStringMaxLength } = require('buffer');
+const { kStringMaxLength } = require('buffer'); 
+
+ const bodyParser = require('body-parser');
+
+    app.use(bodyParser.json())
+    .use(bodyParser.urlencoded({
+        extended: true
+    }));
 
 app.set('view engine', 'ejs');
 
-mongoose.connect('mongodb+srv://admin-marina:marinochka90@cluster0.hr1hl.mongodb.net/moviesDB?retryWrites=true&w=majority');
 
-const moviesSchema = {
-    title: String,
-    genre: String,
-    year: String
-}
+// make a connection 
+mongoose.connect('mongodb+srv://kzhccric:2FQHi2IPGWdllW21@cluster0.v6byg.mongodb.net/kzhcCric?retryWrites=true&w=majority');
+ 
+// get reference to database
+var db = mongoose.connection;
+ 
+db.on('error', console.error.bind(console, 'connection error:'));
 
-const Movie = mongoose.model('Movie', moviesSchema);
+    // define Schema
+    var MatchSchema = mongoose.Schema({
+      name: String,
+      team1name: String,
+      team1flug: String,
+      team1run: String,
+      team2name: String,
+      team2flug: String,
+      team2run: String,
+      status: String,
+      bat1name: String,
+      bat1run: String,
+      bat1ball: String,
+      bat2name: String,
+      bat2run: String,
+      bat2ball: String,
+      ballname: String,
+      ballover: String,
+      ballwk: String
+    });
+
+    // compile schema to model
+    var Match = mongoose.model('Match', MatchSchema, 'Matchstore');
+ 
+
+
+    // define Schema
+    var BookSchema = mongoose.Schema({
+      name: String,
+      price: Number,
+      quantity: Number
+    });
+ 
+    // compile schema to model
+    var Book = mongoose.model('Book', BookSchema, 'bookstore');
+ 
+
+
+
 
 app.get('/', (req, res) => {
-    Movie.find({}, function(err, movies) {
+    Book.find({}, function(err, book) {
+        //console.log(movie)
         res.render('index', {
-            moviesList: movies
+            bookList: book
         })
     })
 })
 
-app.listen(4000, function() {
+app.get('/match', (req, res) => {
+
+
+    Match.find({}, function(err, match) {
+
+    Book.find({}, function(err, book) {
+        res.render('match', {
+            bookList: book,
+	    matchList: match
+        })
+    })
+    })
+})
+
+
+app.post('/addm',(req,res) => {  
+
+	console.log(req.body.name,req.body.team1name,req.body.team2name)
+
+
+    // a document instance
+    var match1 = new Match({ 
+		name: req.body.name , 
+		team1name: req.body.team1name,
+		team1flug: req.body.team1flug,
+		team1run: req.body.team1run,
+		team2name: req.body.team2name,
+		team2flug: req.body.team2flug,
+		team2run: req.body.team2run,
+		status: req.body.status,
+		bat1name: req.body.bat1name,
+		bat1run: req.body.bat1run,
+		bat1ball: req.body.bat1ball,
+		bat2name: req.body.bat2name,
+		bat2run: req.body.bat2run,
+		bat2ball: req.body.bat2ball,
+		ballname: req.body.ballname,
+		ballover: req.body.ballover,
+		ballwk: req.body.ballwk		
+ });
+ 
+    // save model to database
+    match1.save(function (err, match) {
+      if (err) return console.error(err);
+      console.log(req.body.name +  " saved to Match collection.",req.body.team1run);
+	res.redirect('/match');
+    });
+
+
+
+
+})
+
+
+
+
+app.post('/add',(req,res) => {  
+
+	console.log(req.body.name,req.body.price,req.body.quantity)
+
+
+    // a document instance
+    var book1 = new Book({ name: req.body.name , price: req.body.price, quantity: req.body.quantity });
+ 
+    // save model to database
+    book1.save(function (err, book) {
+      if (err) return console.error(err);
+      console.log(req.body.name + " saved to bookstore collection.");
+	res.redirect('/');
+    });
+
+
+
+
+})
+
+
+
+app.post('/del', (req, res) => {
+  res.redirect('/');
+  console.log(req.body.id);
+    Book.remove({_id:req.body.id}, function(err){
+    if(err) throw err;
+});
+
+
+});
+
+
+
+app.post('/delm', (req, res) => {
+  res.redirect('/match');
+  console.log(req.body.id);
+    Match.remove({_id:req.body.id}, function(err){
+    if(err) throw err;
+});
+
+
+});
+
+
+
+
+app.listen(process.env.PORT || 4000, function() {
     console.log('server is running');
 })
+
+
+
+
