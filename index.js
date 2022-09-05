7ddf0a36f2b6d27202f0f807e46785f5b32a143d
kzhctech <95127984+kzhctech@users.noreply.github.com>
2022-09-05 17:35:14 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 
-
 const express = require("express");
+const app = express();
+const product = require("./api/product");
+
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => {
-res.send("hi");});
+  console.error('express connection');
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
+
 
 
 
